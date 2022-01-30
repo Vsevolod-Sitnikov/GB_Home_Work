5373bc5918ba4e4a31773844e5048b210629aa26
Vsevolod-Sitnikov <81462422+Vsevolod-Sitnikov@users.noreply.github.com>
2022-01-30 16:37:49 +0400

    Create 4_lesson_1_task.py

---
+++ 4_lesson_1_task.py
@@ -0,0 +1,11 @@
+import sys
+
+
+work_time = sys.argv[1]
+
+price_one_time = sys.argv[2]
+
+premiya = sys.argv[3]
+
+
+print('Заработная плата сотрудника составляет ', int(work_time)*int(price_one_time)+int(premiya))
