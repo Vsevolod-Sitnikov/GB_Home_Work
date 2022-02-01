8da929e06a9bc11f2e31b4816480e760d5e9a712
Vsevolod-Sitnikov <81462422+Vsevolod-Sitnikov@users.noreply.github.com>
2022-02-01 16:08:18 +0400

    Update 5_lesson_1_task.py

--- 5_lesson_1_task.py
+++ 5_lesson_1_task.py
@@ -1,12 +1,11 @@
 close_flag = True
 
-
 with open('file_1_task.txt', 'w') as file:
     while close_flag:
         write_line = input('введите необходимую запись:\n')
         if write_line == '':
             close_flag = False
-        file.writelines(write_line + '\n')
-
+        else:
+            file.writelines(write_line + '\n')
 
 print('Запись в файл произведена')
