0c5af647a6470a4bf7ecd2e29403e5580d983c26
Vsevolod-Sitnikov <81462422+Vsevolod-Sitnikov@users.noreply.github.com>
2022-02-13 16:32:27 +0400

    Create 7_lesson_3_task.py

---
+++ 7_lesson_3_task.py
@@ -0,0 +1,49 @@
+class Cell:
+
+    def __init__(self, alveola):
+        self.alveola = alveola
+
+    def __add__(self, other):
+        return Cell(self.alveola + other.alveola)
+
+    def __sub__(self, other):
+        if self.alveola - other.alveola > 0:
+            return Cell(self.alveola - other.alveola)
+        else:
+            print("Разниза при вычитании меньше 0")
+
+    def __mul__(self, other):
+        return Cell(self.alveola * other.alveola)
+
+    def __truediv__(self, other):
+        return Cell(int(self.alveola / other.alveola))
+
+    def make_order(self, alveola_count):
+        print('*****\n' * (self.alveola // alveola_count), end='')
+        print('*' * (self.alveola % alveola_count))
+
+nutrion_1 = Cell(123)
+
+nutrion_2 = Cell(89)
+
+nutrion_3 = nutrion_1 + nutrion_2
+
+nutrion_4 = nutrion_1 - nutrion_2
+
+nutrion_5 = nutrion_1 * nutrion_2
+
+nutrion_6 = nutrion_1 / nutrion_2
+
+print(nutrion_1.alveola)
+
+print(nutrion_2.alveola)
+
+print(nutrion_3.alveola)
+
+print(nutrion_4.alveola)
+
+print(nutrion_5.alveola)
+
+print(nutrion_6.alveola)
+
+nutrion_1.make_order(5)
