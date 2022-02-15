38773131f5a4eac6e8271197858ffb470e786598
Vsevolod-Sitnikov <81462422+Vsevolod-Sitnikov@users.noreply.github.com>
2022-02-15 17:14:16 +0400

    Update 8_lesson_1_task.py

--- 8_lesson_1_task.py
+++ 8_lesson_1_task.py
@@ -6,12 +6,6 @@ class Data:
     def __str__(self):
         return f'<Класс Дата>'
 
-    def __doc__(self):
-        """
-        Класс даты. Для создания объекта необходимо ввести дату в формате «день-месяц-год»
-        :return:
-        """
-
     @classmethod
     def data_to_int(cls, data):
         data_list = data.split('-')
@@ -21,11 +15,34 @@ class Data:
         return day, month, year
 
     @staticmethod
-    def valid_data():
-        pass
+    def valid_data(data):
+        month_valid = {
+            '01': 31, 
+            '02': 28, 
+            '03': 31, 
+            '04': 30, 
+            '05': 31, 
+            '06': 30, 
+            '07': 30, 
+            '08': 31, 
+            '09': 30, 
+            '10': 31, 
+            '11': 30, 
+            '12': 31
+        }
+        data_list = data.split('-')
+        for elem in data_list:
+            if elem.isdigit() == False:
+                return print(f"Элемент {elem} не является числом")
+        if int(data_list[0]) > int(month_valid[data_list[1]]) and int(data_list[0]) < 0:
+            return print(f"Некорректная дата. В месяце {month_valid[data_list[1]]} количество дней составляет  {month_valid[data_list[1]].value()}")
+        else:
+            return print('Дата корректна')
 
 my_date = Data('05-02-2021')
 
 print(my_date)
 
 print(Data.data_to_int('05-02-2021'))
+
+Data.valid_data('10-05-2021')
