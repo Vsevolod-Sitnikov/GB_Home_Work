7a05d1404a39906c6090748a8434118bd9472097
Vsevolod-Sitnikov <81462422+Vsevolod-Sitnikov@users.noreply.github.com>
2022-02-16 18:38:16 +0400

    Create 8_lesson_4_task.py

---
+++ 8_lesson_4_task.py
@@ -0,0 +1,20 @@
+class Sklad:
+    def __init__(self):
+        pass
+
+class Orgtekhnika:
+    def __init__(self, color, cost):
+        self.color = color
+        self.cost = cost
+
+class Printer(Orgtekhnika):
+    def __init__(self):
+        pass
+
+class Skaner(Orgtekhnika):
+    def __init__(self):
+        pass
+
+class Xerox(Orgtekhnika):
+    def __init__(self):
+        pass
