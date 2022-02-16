535fe306d07a111eba62d0d2e448e75fb98ca4ab
Vsevolod-Sitnikov <81462422+Vsevolod-Sitnikov@users.noreply.github.com>
2022-02-17 01:43:21 +0400

    Update 8_lesson_4_task.py

--- 8_lesson_4_task.py
+++ 8_lesson_4_task.py
@@ -1,10 +1,38 @@
+from abc import ABC, abstractmethod
+
+
 class Type_Devices_Exception(Exception):
 
     def __init__(self, text):
         self.text = text
 
 
-class Sklad:
+class SkladInterfaces(ABC):
+    '''
+    Интерфейс класса Склад
+    Методы:
+    add_devices - добавить устройство или устройства
+    remove_device - удалить устройство
+    show_inventory - посмотреть какие устройства есть на складе
+    :return:
+    '''
+
+    @abstractmethod
+    def add_devices(self):
+        pass
+
+    @abstractmethod
+    def remove_device(self):
+        pass
+
+    @abstractmethod
+    def show_inventory(self):
+        pass
+
+
+class Sklad(SkladInterfaces):
+
+    __all_cost_devices = int()
 
     def __init__(self):
         self.devices_on_the_sklad = {}
@@ -16,24 +44,29 @@ class Sklad:
                 self.devices_on_the_sklad[serial_number] += 1
             else:
                 self.devices_on_the_sklad[serial_number] = 1
+            Sklad.__all_cost_devices += device.cost
 
     def remove_device(self, device):
         device_serial_number = device.get_serial_number()
         if self.devices_on_the_sklad.get(device_serial_number):
             self.devices_on_the_sklad[device_serial_number] -= 1
             if self.devices_on_the_sklad[device_serial_number] == 0:
-                self.devices_on_the_sklad.pop([device_serial_number])
+                self.devices_on_the_sklad.pop(device_serial_number)
+            Sklad.__all_cost_devices -= device.cost
             return True
         return False
-
 
     def show_inventory(self):
         return self.devices_on_the_sklad
 
-    def show_all_cost(self):
-        all_cost = int()
-        for key, value in devices_on_the_sklad:
-            all_cost += Orgtekhnika.get_cost_device()
+    @classmethod
+    def show_all_cost(cls):
+        return cls.__all_cost_devices
+
+
+class Departament:
+    pass
+
 
 
 class Orgtekhnika:
@@ -56,6 +89,10 @@ class Orgtekhnika:
 
     def get_cost_device(self):
         return self.cost
+
+    @classmethod
+    def count_used_serial_number(cls):
+        return cls.__serial_number
 
 
 class Printer(Orgtekhnika):
@@ -102,4 +139,14 @@ print(new_xerox.parameters())
 
 print(new_sklad.show_inventory())
 
+print(new_sklad.show_all_cost())
+
+new_sklad.remove_device(new_xerox)
+
+print(new_sklad.show_all_cost())
+
+print(new_sklad.show_inventory())
+
 new_printer_for_err = Printer('black', '123', 'HP 2205Z')
+
+print(SkladInterfaces.__doc__)
